041a1a9fa14fbb4705da3be864c87d4651adaf92
hassan402-paymentrequired <175612412+hassan402-paymentrequired@users.noreply.github.com>
2026-03-14 22:43:43 +0000

    Fix styling

--- tests/EnvGuardTest.php
+++ tests/EnvGuardTest.php
@@ -1,23 +1,25 @@
 <?php
 
+use LaramicStudio\EnvGuard\EnvGuard;
+
 it('passes when boolean  is provided', function () {
     config(['env-guard.rules' => ['APP_DEBUG' => 'boolean']]);
     putenv('APP_DEBUG=');
-    (new \LaramicStudio\EnvGuard\EnvGuard)->validate();
+    (new EnvGuard)->validate();
 })->throwsNoExceptions();
 
 it('fails when required key is missing', function () {
     config(['env-guard.rules' => ['APP_KEY' => 'required']]);
     putenv('APP_KEY=');
 
-    expect(fn () => (new \LaramicStudio\EnvGuard\EnvGuard)->validate())
-        ->toThrow(\RuntimeException::class, 'APP_KEY');
+    expect(fn () => (new EnvGuard)->validate())
+        ->toThrow(RuntimeException::class, 'APP_KEY');
 });
 
 it('passes when required key is provided', function () {
     config(['env-guard.rules' => ['APP_KEY' => 'required']]);
     putenv('APP_KEY=test');
-    (new \LaramicStudio\EnvGuard\EnvGuard)->validate();
+    (new EnvGuard)->validate();
 })->throwsNoExceptions();
 
 it('fails when the right type is not provided for the attribute', function () {
@@ -25,9 +27,9 @@ it('fails when the right type is not provided for the attribute', function () {
 
     putenv('APP_NAME=test');
 
-    (new \LaramicStudio\EnvGuard\EnvGuard)->validate();
+    (new EnvGuard)->validate();
 
-})->throws(\RuntimeException::class, 'integer');
+})->throws(RuntimeException::class, 'integer');
 
 it('fails when right value is not provided in the env file provided', function () {
     config(['env-guard.rules' => ['APP_NAME' => ['required', 'in:1,2,3,4,5'], 'APP_KEY' => 'required|starts_with:laravel']]);
@@ -35,5 +37,5 @@ it('fails when right value is not provided in the env file provided', function (
     putenv('APP_NAME=3');
     putenv('APP_KEY=money');
 
-    (new \LaramicStudio\EnvGuard\EnvGuard)->validate();
-})->throws(\RuntimeException::class, 'must');
+    (new EnvGuard)->validate();
+})->throws(RuntimeException::class, 'must');
